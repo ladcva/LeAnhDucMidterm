f7dc6f88c3370ab21266184a80264db48d1ac93d
Le Anh Duc <ladcva@yahoo.com>
2020-11-24 09:56:51 +0700

    Initial Commit

---
+++ LeAnhDucMidterm/PostController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace LeAnhDucMidterm
+{
+    public class PostController
+    {
+        public void AddOnePost(PostModel OnePost)
+        {
+
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["MidTermCon"].ConnectionString))
+            {
+                return;
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = "INSERT into Post (Title, Summary, Content) VALUES (@Title, @Summary, @Content)";
+                    command.Parameters.AddWithValue("@Title", OnePost.Title);
+                    command.Parameters.AddWithValue("@Summary", OnePost.Summary);
+                    command.Parameters.AddWithValue("@Content", OnePost.Content);
+
+                    try
+                    {
+                        connection.Open();
+                        //int recordsAffected = command.ExecuteNonQuery();
+                    }
+                    catch (SqlException)
+                    {
+                        Console.WriteLine("Error Connecting DB !");
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ LeAnhDucMidterm/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace LeAnhDucMidterm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Cnn = ConfigurationManager.ConnectionStrings["MidTermCon"].ConnectionString;
+            string Cmd = "";
+            if (args.Length > 0)
+                Cmd = args[0];
+
+            PostModel OnePost = new PostModel();
+
+            switch (Cmd.ToLower())
+            {
+                case "add":
+                    //Code add here
+                    if (args.Length != 4)
+                    {
+                        Console.WriteLine("usage: Add Title Summary Content");
+                        return;
+                    }
+                    OnePost.Title = args[1];
+                    OnePost.Summary = args[2];
+                    OnePost.Content = args[3];
+
+                    //Write your code to add Title, Sumary, Content into database
+
+                    Console.WriteLine("Title : " + OnePost.Title);
+                    Console.WriteLine("Summary : " + OnePost.Summary);
+                    Console.WriteLine("Content : " + OnePost.Content);
+
+                    PostController insert = new PostController();
+                    insert.AddOnePost(OnePost);
+
+                    break;
+                case "delete":
+
+                    if (args.Length != 2)
+                    {
+                        Console.WriteLine("usage: Delete Id");
+                        return;
+                    }
+                    //Get string from the second paramenters
+                    string StrId = args[1];
+                    //Convert the second parameter into in, stored in Id variable
+                    bool isValid = int.TryParse(StrId, out OnePost.Id);
+                    if (!isValid)
+                    {
+                        Console.WriteLine("Invalid Id " + StrId);
+                        return;
+                    }
+                    /*Code Delete here with the paramenter Id */
+                    if (OnePost.Id > 0)
+                    {
+                        /*write your code here */
+                    }
+
+
+                    Console.WriteLine("Delete completed " + StrId);
+                    break;
+                case "list":
+                    if (args.Length != 2)
+                    {
+                        Console.WriteLine("usage: List Keyword");
+                        return;
+                    }
+                    string Keyword = args[1];
+                    if (string.IsNullOrEmpty(Keyword))
+                    {
+                        //Code search with no keyword
+                    }
+                    else
+                    {
+                        //Code search with keyword
+                    }
+
+
+                    Console.WriteLine("Query Completed " + Keyword);
+                    break;
+                case "Load":
+
+                    break;
+                default:
+                    Console.WriteLine("Invalid command");
+                    break;
+            }
+
+            return;
+        }
+    }
+}
